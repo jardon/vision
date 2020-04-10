b6b71c9148332d4e4fec15ceac245e70fec2f716
Jarred Wilson <jarred.wilson12@gmail.com>
2020-04-10 17:42:26 -0400

    Add commit calendar

--- web/vision/src/components/Dashboard.jsx
+++ web/vision/src/components/Dashboard.jsx
@@ -2,28 +2,44 @@ import React, { Component } from 'react';
 import Repository from './Repository'
 import Search from './Search'
 import axios from 'axios';
+import Box from '@material-ui/core/Box';
 
 
 class Dashboard extends Component {
     state = { 
         data: "No data loaded",
-        input: ""
+        input: "",
+        calendarData: []
      }
 
     updateInput = (field) => this.setState({input: field});
 
-    getData = async (textField) => {
-        this.setState({input: textField});
+    getData = async () => {
+        let data = [];
         let response = await axios.get('https://api.github.com/repos/' + this.state.input);
-        this.setState({data: response.data});
+        let commits = await axios.get(response.data.url + "/stats/commit_activity");
+
+        for(let i = 0; i < 52; i++) {
+            if (commits.data[i].total != 0) {
+                let date = new Date(commits.data[i].week * 1000)
+                for(let j = 0; j < 7; j++) {
+                    date.setDate(date.getDate() + 1);
+                    if(commits.data[i].days[j] != 0) 
+                        data.push({"day": date.toISOString().slice(0, 10), "value": commits.data[i].days[j]})
+                }
+            }
+        }
+        this.setState({data: response.data, calendarData: data});
     };
 
     render() { 
         return ( 
-            <React.Fragment>
-                <Search  search={this.getData.bind(this)} updateInput={this.updateInput.bind(this)}/>
-                <Repository data={this.state.data}/>
-            </React.Fragment>
+            <div style={{"height" : "1200px"}}>
+                <Box height="100%" width="100%">
+                    <Search  search={this.getData.bind(this)} updateInput={this.updateInput.bind(this)}/>
+                    <Repository data={this.state.data} calendarData={this.state.calendarData}/>
+                </Box>
+            </div>
          );
     }
 }
--- web/vision/src/components/Repository.jsx
+++ web/vision/src/components/Repository.jsx
@@ -1,42 +1,78 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-
+import { ResponsiveCalendar } from '@nivo/calendar'
 
 class Repository extends Component {
 
+    date = new Date(1574553600 * 1000);
+    date2 = new Date(1574553600 * 1000);
+
     render() { 
 
-
-        if (this.props.data == "No data loaded")
+        if (this.props.data == "No data loaded") {
+            this.date2.setDate(this.date2.getDate() + 1);
             return (  
                 <div>
                     <pre>{this.props.data}</pre>
                 </div>
-            );
+            );}
         else
             return (
-                <div>
-                    <span><br></br>Name: {this.props.data.name}<br></br> </span>
+                <React.Fragment>
+                    <span><br></br>{this.props.data.owner.login}/{this.props.data.name}<br></br> </span>
                     <span>Id: {this.props.data.id}<br></br> </span>
-                    <span>Owner: {this.props.data.owner.login}<br></br> </span>
-                </div>
+                    <h1>Commits</h1>
+                     <ResponsiveCalendar
+                        data={this.props.calendarData}
+                        from="2019-04-10"
+                        to={new Date().toISOString().slice(0, 10)}
+                        emptyColor="#eeeeee"
+                        colors={[ '#4791db', '#1976d2', '#115293', '#dc004e' ]}
+                        margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+                        yearSpacing={40}
+                        monthBorderColor="#ffffff"
+                        dayBorderWidth={2}
+                        dayBorderColor="#ffffff"
+                        legends={[
+                            {
+                                anchor: 'bottom-right',
+                                direction: 'row',
+                                translateY: 36,
+                                itemCount: 4,
+                                itemWidth: 42,
+                                itemHeight: 36,
+                                itemsSpacing: 14,
+                                itemDirection: 'right-to-left'
+                            }
+                        ]}
+                    />
+                    {/*<h1>Issues</h1>
+                    <ResponsiveCalendar
+                        data={this.data}
+                        from="2019-04-10"
+                        to={new Date().toISOString().slice(0, 10)}
+                        emptyColor="#eeeeee"
+                        colors={[ '#4791db', '#1976d2', '#115293', '#dc004e' ]}
+                        margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+                        yearSpacing={40}
+                        monthBorderColor="#ffffff"
+                        dayBorderWidth={2}
+                        dayBorderColor="#ffffff"
+                        legends={[
+                            {
+                                anchor: 'bottom-right',
+                                direction: 'row',
+                                translateY: 36,
+                                itemCount: 4,
+                                itemWidth: 42,
+                                itemHeight: 36,
+                                itemsSpacing: 14,
+                                itemDirection: 'right-to-left'
+                            }
+                        ]}
+                    /> */}
+                </React.Fragment>
             );
     }
 }
 
 export default Repository;
-
-// class Child1 extends Component{
-//     childFunction=(e)=>{
-//         e.preventDefault();
-//         this.props.functionCallFromParent("Hello From Child1");
-//     }
-//     render(){
-//         return(
-//             <div>   
-//                 <button onClick={this.childFunction.bind(this)}>Click</button>
-//             </div>
-//         );
-//     }
-// }
-// export default Child1;
