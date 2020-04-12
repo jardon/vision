8c26946eeb7f0820ab35353ee1c5f19d86c4a7ee
Jarred Wilson <jarred.wilson12@gmail.com>
2020-04-12 09:34:08 -0400

    Remove no longer needed comment

--- web/vision/src/components/Search.js
+++ web/vision/src/components/Search.js
@@ -15,18 +15,3 @@ class Search extends Component {
 }
  
 export default Search;
-
-// class Child1 extends Component{
-//     childFunction=(e)=>{
-//         e.preventDefault();
-//         this.props.functionCallFromParent("Hello From Child1");
-//     }
-//     render(){
-//         return(
-//             <div>   
-//                 <button onClick={this.childFunction.bind(this)}>Click</button>
-//             </div>
-//         );
-//     }
-// }
-// export default Child1;
