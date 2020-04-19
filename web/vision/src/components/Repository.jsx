5c33c3fb8dd714c67b73b0ba873df0c2b9aca162
Jarred Wilson <jarred.wilson12@gmail.com>
2020-04-18 20:36:43 -0400

    Rework dynamic repository component

--- web/vision/src/components/Repository.jsx
+++ web/vision/src/components/Repository.jsx
@@ -14,38 +14,7 @@ class Repository extends Component {
                     <pre>{this.props.data}</pre>
                 </div>
             );}
-        else if(this.props.issueData == null) {
-            
-            return (
-                <React.Fragment>
-                    <h1>Commits</h1>
-                     <ResponsiveCalendar
-                        data={this.props.commitData}
-                        from={begin.toISOString().slice(0, 10)}
-                        to={end.toISOString().slice(0, 10)}
-                        emptyColor="#eeeeee"
-                        colors={[ '#4791db', '#1976d2', '#115293', '#dc004e' ]}
-                        margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
-                        yearSpacing={40}
-                        monthBorderColor="#ffffff"
-                        dayBorderWidth={2}
-                        dayBorderColor="#ffffff"
-                        legends={[
-                            {
-                                anchor: 'bottom-right',
-                                direction: 'row',
-                                translateY: 36,
-                                itemCount: 4,
-                                itemWidth: 42,
-                                itemHeight: 36,
-                                itemsSpacing: 14,
-                                itemDirection: 'right-to-left'
-                            }
-                        ]}
-                    />
-                </React.Fragment>
-            );}
-        else 
+        else {
             return (
                 <React.Fragment>
                     <h1>Commits</h1>
@@ -73,33 +42,36 @@ class Repository extends Component {
                             }
                         ]}
                     />
-                    <h1>Issues</h1>
-                    <ResponsiveCalendar
-                        data={this.props.issueData}
-                        from={begin.toISOString().slice(0, 10)}
-                        to={end.toISOString().slice(0, 10)}
-                        emptyColor="#eeeeee"
-                        colors={['#61cdbb', '#97e3d5', '#e8c1a0', '#f47560']}
-                        margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
-                        yearSpacing={40}
-                        monthBorderColor="#ffffff"
-                        dayBorderWidth={2}
-                        dayBorderColor="#ffffff"
-                        legends={[
-                            {
-                                anchor: 'bottom-right',
-                                direction: 'row',
-                                translateY: 36,
-                                itemCount: 4,
-                                itemWidth: 42,
-                                itemHeight: 36,
-                                itemsSpacing: 14,
-                                itemDirection: 'right-to-left'
-                            }
-                        ]}
-                    />
+                    {this.props.issueData != null && <React.Fragment>
+                        <h1>Issues</h1>
+                        <ResponsiveCalendar
+                            data={this.props.issueData}
+                            from={begin.toISOString().slice(0, 10)}
+                            to={end.toISOString().slice(0, 10)}
+                            emptyColor="#eeeeee"
+                            colors={['#61cdbb', '#97e3d5', '#e8c1a0', '#f47560']}
+                            margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+                            yearSpacing={40}
+                            monthBorderColor="#ffffff"
+                            dayBorderWidth={2}
+                            dayBorderColor="#ffffff"
+                            legends={[
+                                {
+                                    anchor: 'bottom-right',
+                                    direction: 'row',
+                                    translateY: 36,
+                                    itemCount: 4,
+                                    itemWidth: 42,
+                                    itemHeight: 36,
+                                    itemsSpacing: 14,
+                                    itemDirection: 'right-to-left'
+                                }
+                            ]}
+                        />
+                    </React.Fragment>}
                 </React.Fragment>
             );
+        }
     }
 }
 
